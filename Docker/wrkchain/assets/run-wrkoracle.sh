0d25fb9231e723f2bf881f39838fe943ddeaeaf3
Codegnosis <paul@codegnosis.co.uk>
2019-05-29 14:45:09 +0100

    Docker environment for UND devnet and WRKChain

---
+++ Docker/wrkchain/assets/run-wrkoracle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 20
+
+wrkoracle register --password /root/.wrkchain_oracle/.password --account 0x83197E2E58929c41f6Ebe3b4eA6D6a07Cfb4A97B --genesis /root/.wrkchain_oracle/genesis.json --auth 0x83197E2E58929c41f6Ebe3b4eA6D6a07Cfb4A97B --mainchain.rpc "http://172.25.1.5:8101"
+
+sleep 10
+
+wrkoracle record --password /root/.wrkchain_oracle/.password --account 0x83197E2E58929c41f6Ebe3b4eA6D6a07Cfb4A97B --mainchain.rpc "http://172.25.1.5:8101" --wrkchain.rpc "http://172.25.1.25:8547" --hash.parent --hash.receipt --hash.tx --hash.state --freq 10
