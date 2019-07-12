b91ae9f1a8154cf4e32654c3eb37308a0ccadb19
Waleed <waleedpahome@live.com>
2019-07-12 02:30:25 -0400

    added IEXcloud as third data provider

--- Docker/wrkchain/assets/finchain-oracle-service/src/oracle.js
+++ Docker/wrkchain/assets/finchain-oracle-service/src/oracle.js
@@ -5,9 +5,11 @@ https://www.alphavantage.co/documentation/
 https://rapidapi.com/category/Finance
 https://www.programmableweb.com/news/96-stocks-apis-bloomberg-nasdaq-and-etrade/2013/05/22
 
+*******THE FOLLOWING KEYS SHOULD DEFINITELY BE STORED AS ENV VARS!!!**************
 alphavantage API key: PC5LQPAGBFVN0E07
 worldtradingdata API key: gA38Z0zV0GKDoNKNxFqb82sWYSarX7bNvcrTm0mC0irfnEfYdr8fm1qa7ID4
-
+IEX API secret key : sk_1382db890cfe460bb10c65f658c69dd6
+IEX API key : pk_9d147f681d434a19b6bad93e80bacf0a
 */
 
 require("dotenv").config();
@@ -23,70 +25,53 @@ var symbols = ['ATVI','ADBE', 'AMD', 'ALXN','ALGN','GOOGL','GOOG','AMZN','AAL','
     'TXN','KHC','ULTA','UAL','VRSN','VRSK',	'VRTX',	'WBA','WDAY','WDC','WLTW','WYNN','XEL','XLNX'];
 
 function alphaVantageApi() {
-
     var alpha_key = 'PC5LQPAGBFVN0E07';
     var alpha_uri = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+symbols[0]+'&interval=60min&apikey='+alpha_key;
 
     request.get(alpha_uri, function (error, response, body) {
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    if (error) console.log('error:', error); // Print the error if one occurred
-    else {  
-        var myJson = JSON.parse(body);
-        var price = myJson["Time Series (60min)"];
-        for (x in price){
-            console.log(price[x]['4. close']); //print JSON body
-        }   
-    }
-  });
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        if (error) console.log('error:', error); // Print the error if one occurred
+        else {  
+            var myJson = JSON.parse(body);
+            var price = myJson["Time Series (60min)"];
+            for (x in price){
+                console.log(price[x]['4. close']); //print JSON body
+            }   
+        }
+    });
 }
 
 function worldTradingDataApi() {
-    var world_key = 'PC5LQPAGBFVN0E07';
+    var world_key = 'gA38Z0zV0GKDoNKNxFqb82sWYSarX7bNvcrTm0mC0irfnEfYdr8fm1qa7ID4';
     var world_uri = 'https://api.worldtradingdata.com/api/v1/stock?symbol='+symbols[0]+'&api_token='+world_key;
 
-    request.get(world_uri, function (error, response, body) {
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    if (error) console.log('error:', error); // Print the error if one occurred
-    else {  
-        var myJson = JSON.parse(body);
-        var price = myJson["Time Series (60min)"];
-        for (x in price){
-            console.log(price[x]['4. close']); //print JSON body
+    function worldrequest(error, response, body) {
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        if (error) console.log('error:', error); // Print the error if one occurred
+        else {  
+            var myJson = JSON.parse(body);
+            console.log(myJson);
         }   
     }
-  });
-
+    request.get(world_uri,worldrequest);
 }
+
+
+function IEXApi () {
+    var IEX_key = 'pk_9d147f681d434a19b6bad93e80bacf0a';
+    var IEX_uri = 'https://cloud.iexapis.com/stable/tops?token='+IEX_key+'&symbols='+symbols[0];
+
+    request.get(IEX_uri, function (error, response, body) {
+        //console.log("statusCode: ", response);
+        if (error) console.log("error: ", error);
+        else {
+            console.log(body);
+        }
+
+    });
+}
+
+
 /*
-
-const parseData = (body) => {
-  return new Promise((resolve, reject) => {
-    let weatherDescription, temperature, humidity, visibility, pressure, sunrise, sunset;
-    try {
-      let weather = '{' +
-        '"description": "' + body.weather[0].description.toString() + '",' +
-        '"icon": "' + body.weather[0].icon.toString() + '",' +
-        '"id": "' + body.weather[0].id.toString() + '",' +
-        '"main": "' + body.weather[0].main.toString() + '",' +
-        '"location": "' + body.name.toString() + '",' +
-        '"country": "' + body.sys.country.toString() + '"' +
-      '}';
-      console.log(weather);
-      weatherDescription = weather;
-      temperature = body.main.temp.toString();
-      humidity = body.main.humidity.toString();
-      visibility = body.visibility.toString();
-      pressure = body.main.pressure.toString();
-      sunrise = body.sys.sunrise.toString();
-      sunset = (body.sys.sunset || 0).toString();
-    } catch(error) {
-      reject(error);
-      return;
-    }
-    resolve({ weatherDescription, temperature, humidity, visibility, pressure, sunrise, sunset });
-  });
-};
-
-
 setTimeout(); //useful for continually executing a piece of code on an interval
 */
