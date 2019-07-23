110d73275ad9a2a6bf8981e2d0618c65104015e5
Waleed <waleedpahome@live.com>
2019-07-23 13:25:43 -0400

    Reordered env vars and packages

--- Docker/wrkchain/assets/finchain-oracle-contract/truffle-config.js
+++ Docker/wrkchain/assets/finchain-oracle-contract/truffle-config.js
@@ -18,7 +18,7 @@
  *
  */
 
-require("dotenv").config();
+require('dotenv').config({ path: 'C:/Users/Waleed Elsakka/Documents/Bounties/finchain-demo/.env'});
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
@@ -29,38 +29,13 @@ exports.privateKeys = [
   "5d0929559ea34a21cfece4183400ebfbb049f7c8a7cb251b61a4b6cde399ef42"
 ];
 
-exports.rpcport = "http://localhost:8547";
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
-
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-
-
        development: {
         host: "localhost",     // Localhost (default: none)
         port: 8547,            // Standard Ethereum port (default: none)
         network_id: 2339117895,       // Any network (default: none)
-        provider: new HDWalletProvider(this.privateKeys, this.rpcport , 0 , 4),
+        provider: new HDWalletProvider(this.privateKeys, process.env.WRKCHAIN_RPC_PORT , 0 , 4),
         gas: 4500000,
        },
 
