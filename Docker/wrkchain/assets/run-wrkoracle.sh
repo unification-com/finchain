6b9a25e625f7c31f747de091a33548e2761dd184
Codegnosis <paul@codegnosis.co.uk>
2019-07-25 14:41:20 +0100

    docker - fix times and IPs in .envs and scripts

--- Docker/wrkchain/assets/run-wrkoracle.sh
+++ Docker/wrkchain/assets/run-wrkoracle.sh
@@ -4,8 +4,8 @@ SUBNET_IP=$1
 
 sleep 20
 
-wrkoracle register --password /root/.wrkchain_oracle/.password --account 0x83197E2E58929c41f6Ebe3b4eA6D6a07Cfb4A97B --genesis /root/.wrkchain_oracle/genesis.json --auth 0x83197E2E58929c41f6Ebe3b4eA6D6a07Cfb4A97B --mainchain.rpc "http://$SUBNET_IP.15:8101"
+wrkoracle register --password /root/.wrkchain_oracle/.password --account 0x83197E2E58929c41f6Ebe3b4eA6D6a07Cfb4A97B --genesis /root/.wrkchain_oracle/genesis.json --auth 0x83197E2E58929c41f6Ebe3b4eA6D6a07Cfb4A97B --mainchain.rpc "http://$SUBNET_IP.12:8101"
 
-sleep 10
+sleep 60
 
-wrkoracle record --password /root/.wrkchain_oracle/.password --account 0x83197E2E58929c41f6Ebe3b4eA6D6a07Cfb4A97B --mainchain.rpc "http://$SUBNET_IP.15:8101" --wrkchain.rpc "http://$SUBNET_IP.25:8547" --hash.parent --hash.receipt --hash.tx --hash.state --freq 30
+wrkoracle record --password /root/.wrkchain_oracle/.password --account 0x83197E2E58929c41f6Ebe3b4eA6D6a07Cfb4A97B --mainchain.rpc "http://$SUBNET_IP.12:8101" --wrkchain.rpc "http://$SUBNET_IP.25:8547" --hash.parent --hash.receipt --hash.tx --hash.state --freq 60
